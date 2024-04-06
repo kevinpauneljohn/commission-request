e287c85b4591787797a603c567e497249a85919a
kevinpauneljohn <johnkevinpaunel@gmail.com>
2024-04-06 20:56:18 +0800

    update task to completed

---
+++ app/Console/Commands/CompleteTasksWhenRequestIsCompleted.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Request;
+use App\Models\Task;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class CompleteTasksWhenRequestIsCompleted extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:tasks';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update all tasks when request is completed';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $requests = collect(Request::where('status','completed')->get())->pluck('id')->toArray();
+        if(DB::table('tasks')->whereIn('request_id',$requests)->where('status','pending')->update(['status' => 'completed']))
+            Log::info('all tasks completed');
+    }
+}
