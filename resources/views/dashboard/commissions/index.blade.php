3db819d5c92044c147d8f45fad075570a70b7af8
kevinpauneljohn <johnkevinpaunel@gmail.com>
2024-04-07 14:31:23 +0800

    request progress bar

--- resources/views/dashboard/commissions/index.blade.php
+++ resources/views/dashboard/commissions/index.blade.php
@@ -46,7 +46,7 @@
                             <th>Requester</th>
                         @endif
                         <th>Parent #</th>
-                        <th style="width: 20%;">Progress</th>
+                        <th style="width: 40%;">Progress</th>
                         <th>% Released</th>
                         <th>total %</th>
                         <th>Status</th>
