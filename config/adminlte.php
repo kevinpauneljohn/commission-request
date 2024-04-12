2edd99e7355339c5ce26aeee242a3f146919533d
kevinpauneljohn <johnkevinpaunel@gmail.com>
2024-04-12 22:25:37 +0800

    add change password

--- config/adminlte.php
+++ config/adminlte.php
@@ -258,8 +258,8 @@ return [
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => false,
-    'password_reset_url' => false,
-    'password_email_url' => false,
+    'password_reset_url' => 'password/reset',
+    'password_email_url' => 'password/email',
     'profile_url' => false,
 
     /*
